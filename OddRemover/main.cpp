3fb3c055373009788e52cec30462d0ce443aa2d6
Nyi Htet <nyihtet4dev@icloud.com>
2023-03-18 21:53:58 -0700

    first commit

--- OddRemover/main.cpp
+++ OddRemover/main.cpp
@@ -1,14 +1,28 @@
-//
-//  main.cpp
-//  OddRemover
-//
-//  Created by Nyi Htet on 3/18/23.
-//
+#include<iostream>
+using namespace std;
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+   int a[]= {28, 93, 72, 63, 86, 90, 64};
+   int i = 0;
+int j = 0;
+int size = 7; // size of the array
+for(i = 0; i < size; i++)
+{
+   if (a[i] % 2 == 0) // if a[i] should not be removed
+   {
+      if (i != j)
+      {
+         a[j] = a[i];
+      }
+      j++;
+   }
+}
+size = j;
+   for(int i = 0; i < 10; i++)
+   {
+      cout << a[i] << " ";
+   }
+   cout << endl;
+   return 0;
 }
