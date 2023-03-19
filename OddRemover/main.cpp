5c29ac0ab86f12ce42237f53c3e8663d7c863f36
Nyi Htet <nyihtet4dev@icloud.com>
2023-03-18 21:53:19 -0700

    Initial Commit

---
+++ OddRemover/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OddRemover
+//
+//  Created by Nyi Htet on 3/18/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
